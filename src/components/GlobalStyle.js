d1d02751b2fe9af3116ecb0c65a71c28086f0fd9
최유리 <choiyr5256@gmail.com>
2023-09-11 13:29:07 +0900

    수정본

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components'
+
+
+
+const GlobalStyle =
+createGlobalStyle`
+@font-face {
+    font-family: 'HakgyoansimWoojuR';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2307-2@1.0/HakgyoansimWoojuR.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+}
+
+    *{margin: 0; padding: 0; font-family: 'HakgyoansimWoojuR';}
+    ul{list-style:none}
+    a{text-decoration: none; color: #000}
+`
+
+export default GlobalStyle
